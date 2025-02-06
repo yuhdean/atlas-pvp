3f68a65d5c98941792be43ed89df14678359e356
48dean <deannobusiness@gmail.com>
2025-02-05 20:54:23 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,19 +1,6 @@
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(0) -- loop continuously
-        
-        SetEntityInvincible(PlayerPedId(), false) 
-        SetEntityCanBeDamaged(PlayerPedId(), true) 
-        
-        local ped = PlayerPedId()
-        local nearbyPlayers = GetActivePlayers()
-        
-        for _, playerId in ipairs(nearbyPlayers) do
-            local targetPed = GetPlayerPed(playerId)
-            if ped ~= targetPed then
-                SetEntityCanBeDamaged(targetPed, true)
-                SetEntityInvincible(targetPed, false)
-            end
-        end
+        Citizen.Wait(0) -- Loop continuously
+        SetCanAttackFriendly(PlayerPedId(), true, true) -- Enable PvP
     end
 end)
